15d477e82130dca9b0ac001b2f7bfed5e0b7f3d5
SahasraRReddy <114499019+SahasraRReddy@users.noreply.github.com>
2024-05-06 16:28:43 +0530

    Add files via upload

---
+++ C101/roll-a-dice.py
@@ -0,0 +1,129 @@
+import random
+
+ask = input("Do you want to roll a dice? (y/n) ")
+
+response = "y"
+
+if response == "y":
+
+    no = random.randint(1, 6)
+
+    if no == 1 :
+        print("[---------]")
+        print("[         ]")
+        print("[    0    ]")
+        print("[         ]")
+        print("[---------]")
+
+    if no == 2 :
+        print("[---------]")
+        print("[         ]")
+        print("[   0 0   ]")
+        print("[         ]")
+        print("[---------]")
+
+    if no == 3 :
+        print("[---------]")
+        print("[         ]")
+        print("[  0 0 0  ]")
+        print("[         ]")
+        print("[---------]")
+
+    if no == 4 :
+        print("[---------]")
+        print("[  0  0   ]")
+        print("[         ]")
+        print("[  0  0   ]")
+        print("[---------]")
+
+    if no == 5 :
+        print("[---------]")
+        print("[  0   0  ]")
+        print("[    0    ]")
+        print("[  0   0  ]")
+        print("[---------]")
+
+    if no == 6 :
+        print("[---------]")
+        print("[  0 0 0  ]")
+        print("[         ]")
+        print("[  0 0 0  ]")
+        print("[---------]")
+
+   
+again = input("press y to roll again and n to exit: ")
+
+again == "y"
+
+
+if again == "y":
+    no = random.randint(1, 6)
+
+    if no == 1 :
+        print("[---------]")
+        print("[         ]")
+        print("[    0    ]")
+        print("[         ]")
+        print("[---------]")
+
+    if no == 2 :
+        print("[---------]")
+        print("[         ]")
+        print("[   0 0   ]")
+        print("[         ]")
+        print("[---------]")
+
+    if no == 3 :
+        print("[---------]")
+        print("[         ]")
+        print("[  0 0 0  ]")
+        print("[         ]")
+        print("[---------]")
+
+    if no == 4 :
+        print("[---------]")
+        print("[  0  0   ]")
+        print("[         ]")
+        print("[  0  0   ]")
+        print("[---------]")
+
+    if no == 5 :
+        print("[---------]")
+        print("[  0   0  ]")
+        print("[    0    ]")
+        print("[  0   0  ]")
+        print("[---------]")
+
+    if no == 6 :
+        print("[---------]")
+        print("[  0 0 0  ]")
+        print("[         ]")
+        print("[  0 0 0  ]")
+        print("[---------]")
+
+        
+
+
+
+else:print("exited")
+
+ 
+  
+   
+  
+   
+   
+        
+
+
+
+
+
+
+        
+
+
+
+
+   
+
